92e0d040522260122022f0d658d46960cd9e16cb
GilbertoHdz <ghernandez.9002@gmail.com>
2017-02-09 23:13:27 -0600

    Log: SaveLog in DB

--- src/main/java/com/manitos/component/RequestTimeInterceptor.java
+++ src/main/java/com/manitos/component/RequestTimeInterceptor.java
@@ -1,12 +1,20 @@
 package com.manitos.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.manitos.repository.LogRepository;
 
 /** @author ZubZero
  * Description: Handler que captura el tiempo de request y response.
@@ -15,6 +23,10 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+	
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
 	
 	//PRIMERO
 	@Override
@@ -31,7 +43,17 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 			throws Exception {
 		// TODO Auto-generated method stub
 		long startTime = (long) request.getAttribute("startTime");
-		LOG.info("URL to: '" + request.getRequestURL().toString() + "' In: '" + (System.currentTimeMillis() - startTime) + "ms'");
+		String url = request.getRequestURL().toString();
+		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = "";
+		
+		if(null != auth && auth.isAuthenticated()){
+			username = auth.getName();
+		}
+		
+		logRepository.save(new com.manitos.entity.Log(new Date(), auth.getDetails().toString(), username, url));
+		LOG.info("URL to: '" + url + "' In: '" + (System.currentTimeMillis() - startTime) + "ms'");
 	}
 	
 }
